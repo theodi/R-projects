65143e1609128c8059396cf2da20283fcf7827fa
ulrich <ulrich.atz@theodi.org>
2013-10-07 13:40:52 +0100

    No more git submodules

--- playground/R cartogram.R
+++
@@ -1,6 +0,0 @@
-library(maptools)
-cartogram<-readShapePoly("/Users/Ulrich/Google Drive/ODI Drive/Commercial/Training/Clients and partners/Scottish ONS/part 2/r_cartogram/world_r_carto.shp")
-
-r.cartogram <- readShapePoly("world_r_carto.shp")
-plot(r.cartogram)
-title(main="R Activity Around the World", sub="Based on cran.rstudio.com Activity Logs October 2012-June 2013")
