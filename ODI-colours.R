aa465e0230081dcae3c0d13c86eb96625af6fb5c
ulrich <ulrich.atz@theodi.org>
2014-01-10 10:44:18 +0000

    Adds the ODI colour scheme to R

---
+++ ODI-colours.R
@@ -0,0 +1,15 @@
+# ODI colour scheme
+# source('~/git/R-projects/ODI-colours.R')
+
+odi_dBlue <- "#2254F4"
+odi_mBlue <- "#00B7FF"
+odi_lBlue  <-  "#08DEF9"
+odi_turquoise <- "#1DD3A7"
+odi_dGreen <- "#0DBC37"
+odi_lGreen <- "#67EF67"
+odi_yellow <- "#F9BC26"
+odi_orange <- "#FF6700"
+odi_red <- "#D60303"
+odi_lPink <- "#EF3AAB"
+odi_dPink <- "#E6007C"
+odi_purple <- "#B13198"
