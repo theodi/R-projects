3b803467712fe5d1d781d365128cafad500664f0
ulrich <ulrich.atz@theodi.org>
2014-02-05 11:08:19 +0000

    Keep for legacy code but superseded

--- csv-stats/simple-analysis.R
+++ csv-stats/simple-analysis.R
@@ -8,20 +8,30 @@ source("~/git/R-projects/data-portal-analysis/functions.r")
 
 
 # Probably move elsewhere and 'source'
-clean.data <- function(dta, col) {
-  dta[dta[, col] == "", col] <- NA # set empty cells to missing
-} 
+# replace.empty <- function(dta, col) {
+#   dta[dta[, col] == "", col] <- NA # set empty cells to missing
+# } 
 
 # data.table is lower case, may not be convention
-GOV <- read.csv("/Users/Ulrich/git/csv_finder/csv_urls.csv")
-gov <- as.data.table(gov)
-clean.data(gov, "last_modified")
+gov <- as.data.table(read.csv("/Users/Ulrich/git/csv_finder/csv_urls.csv"))
+# Count missing values, count empty fields
+sapply(gov, no.missing)
+sapply(gov, count.empty)
+
+# WHY is this not working?!
+# replace.empty(gov, "last_modified")
+# gov[gov[, last_modified] == "", last_modified] <- NA
+# Workaround
+gov$last_modified[nchar(gov$last_modified) == 0]  <-  NA
+gov$description[nchar(gov$description) == 0]  <-  NA
 
 # Count unique entries
 sapply(gov, function(x) length(unique(x)))
 
-# Count missing values
-sapply(gov, nmissing)
+# Count missing values, count empty fields - after cleaning
+sapply(gov, no.missing)
+sapply(gov, count.empty)
+
 
 # Count how many csv-files actually have a URL ending in .csv
 table(str_detect(gov$url, "csv$"))
