51581ebc897a53713867c5810d166e667e445c62
ulrich <ulrich.atz@theodi.org>
2014-04-15 17:46:15 +0100

    Some new lines

--- playground/certificates-extract-questions.R
+++ playground/certificates-extract-questions.R
@@ -8,7 +8,12 @@ certificates <- fromJSON(getURL(url))
 
 #Extract list of question names from JSON dataframe
 questions <- lapply(names(certificates), function(x) certificates[[x]][[1]])
-questions <- unlist(questions)
+questions <- as.vector(unlist(questions))
+
+#Extract responses as last item in list (reverse function)
+responses <- lapply(names(certificates), function(x) rev(certificates[[x]])[[1]])
+responses <- as.vector((responses))
+
 
 #Test write to text file
 writeLines(questions, 'cert-questions.txt')
