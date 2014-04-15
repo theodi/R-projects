79a427bb54947a147d1fadb3ef1ffbf27e79fb7c
ulrich <ulrich.atz@theodi.org>
2014-04-15 17:48:37 +0100

    Argh case sensitive names

--- playground/plotly-test.R
+++
@@ -1,5 +0,0 @@
-setwd("~/git/R-projects/playground/")
-source('sensitive-data-never-commit.R')
-library(plotly)
-
-p <- plotly(username = 'statshero', key = plotlykey)
--- playground/reading-ssl-csv.R
+++
@@ -1,4 +0,0 @@
-temporaryFile <- tempfile()
-download.file("https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/270310/UnclaimedEstatesList.csv",
-              destfile = temporaryFile, method = "curl")
-data <- read.csv(temporaryFile)
