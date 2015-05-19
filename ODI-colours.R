a0f3634ffaa7c30503db05540a3cb894d73bd573
Jmerules <Jamie@Jamies-MacBook-Air.local>
2015-05-19 12:20:58 +0100

    add current colours
    updated colours and added pallettes

--- ODI-colours.R
+++ ODI-colours.R
@@ -1,17 +1,44 @@
 # ODI colour scheme
 # source('~/git/R-projects/ODI-colours.R')
-# TODO: store in some list
+
+
+#Primary colours
 
 odi_dBlue <- "#2254F4"
-odi_mBlue <- "#00B7FF"
+odi_mdBlue <- "#178CFF"
+odi_mBlue <- "#00B6FF"
 odi_lBlue  <-  "#08DEF9"
 odi_turquoise <- "#1DD3A7"
 odi_dGreen <- "#0DBC37"
 odi_lGreen <- "#67EF67"
+
+odi_spectrum1 <- c(odi_dBlue, odi_mdBlue, odi_mBlue, odi_lBlue,
+                   odi_turquoise, odi_dGreen, odi_lGreen)
+
 odi_yellow <- "#F9BC26"
 odi_orange <- "#FF6700"
 odi_red <- "#D60303"
+odi_crimson <- "#D73058"
 odi_lPink <- "#EF3AAB"
 odi_dPink <- "#E6007C"
-odi_purple <- "#B13198"
+odi_purple <- "#722EA5"
 
+odi_spectrum2 <- c(odi_yellow, odi_orange, odi_red, odi_crimson, odi_lPink, odi_dPink, odi_purple)
+
+#primary colour pallette
+odi_pallette1 <- c(odi_spectrum1, odi_spectrum2)
+
+#Secondary colours
+
+odi_black <- "#000000" #this is just black
+odi_dGrey <- "#333333"
+odi_lGrey <- "#999999"
+odi_2dBlue <- "#2F539F"
+odi_2mBlue <- "#00ACE8"
+odi_2pBlue <- "#7DC5EA"
+
+#secondary colour pallette
+odi_pallette2 <- c(odi_black, odi_dGrey, odi_lGrey, odi_2dBlue, odi_2mBlue, odi_2pBlue)
+
+#odi full pallette
+odi_pallette_full <- c(odi_pallette, odi_pallette2)
